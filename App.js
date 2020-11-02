c4e4924348b3b5c5dd21264f2cce68fbeb2d6ad6
laith <mr.laith_2011@hotmail.com>
2020-11-02 22:22:06 +0200

    finishlab41

--- App.js
+++ App.js
@@ -1,15 +1,100 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, FlatList, Text, View, Button, Linking, TouchableOpacity } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import * as Contacts from 'expo-contacts';
+import { Camera } from 'expo-camera';
 
-export default function App() {
+function App(props) {
+
+  const [contacts, setContacts] = useState([]);
+  const [permissions, setPermissions] = useState(false);
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  const getPermissions = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CONTACTS);
+    setPermissions(true);
+  };
+
+  const showContacts = async () => {
+    const contactList = await Contacts.getContactsAsync();
+    setContacts(contactList.data);
+  };
+
+
+  useEffect(() => {
+    getPermissions();
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+
+
+  const call = contact => {
+    let phoneNumber = contact.phoneNumbers[0].number.replace(/[\(\)\-\s+]/g, '');
+    console.log(contact.phoneNumbers)
+    let link = `tel:${phoneNumber}`;
+    Linking.canOpenURL(link).then(supported => Linking.openURL(link)).catch(console.error);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>hello laith!</Text>
+      <Button
+        onPress={showContacts}
+        title="show Contacts"
+      />
+     
       <StatusBar style="auto" />
+
+      <View style={styles.section}>
+        <Text>Data ..</Text>
+        <FlatList
+          data={contacts}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <Button style={styles.person} title={item.name} onPress={() => call(item)} />}
+        />
+      </View>
+
+      <View style={{ flex: 1 }}>
+        <Camera style={{ flex: 1 }} type={type}>
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: 'transparent',
+              flexDirection: 'row',
+            }}>
+            <TouchableOpacity
+              style={{
+                flex: 0.1,
+                alignSelf: 'flex-end',
+                alignItems: 'center',
+              }}
+              onPress={() => {
+                setType(
+                  type === Camera.Constants.Type.back
+                    ? Camera.Constants.Type.front
+                    : Camera.Constants.Type.back
+                );
+              }}>
+              <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
+            </TouchableOpacity>
+          </View>
+        </Camera>
+      </View>
     </View>
-    
   );
+
 }
 
 const styles = StyleSheet.create({
@@ -19,4 +104,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  person: {
+    marginTop: '1em',
+  },
+  section: {
+    margin: 10,
+    flex: 1,
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  },
 });
+
+export default App;
--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # reac-native
+
+## End User documentation
+```
+as a User, you can give the app permission to access your contacts information to be able to use it during the use of our app
+when u open our app it will ask you permission to access contact information alow to us to access.
+```
+
+
+### Features
+
+---
+Number and name of feature: expo cli
+Estimate of time needed to complete: 3 hours 
+Start time: 16:00
+Finish time: 19:00
+Actual time needed to complete: 4 hours
+---
+
+
+Date published
+
+```
+---
+date: 2020-9-7 10:00
+---
+```
+
+```
+author: Laith Al Daree
+---
+```
+
+### location
+```
+---
+location: Az Zarqa-Al Azraq-Jordan
+---
+```
+
+### Contact 
+```
+---
+Email:mr.laith_2011@hotmail.com
+GitHub:https://github.com/laith-2020
+linkedIn :https://www.linkedin.com/in/laith-aldaree/
+---
+```
+
+* if you have any quastion feel free to send me an 
+  email : mr.laith_2011@hotmail.com
+
+
+## Dimo Online Url : 
+
+[https://snack.expo.io/@laith-2020/lab41](https://snack.expo.io/@laith-2020/lab41)
+
+## Live Dimo : 
+[https://drive.google.com/file/d/1cxLV4Pl66K7tSCXRcYLg_wsBUM3qk3hn/view?usp=sharing](https://drive.google.com/file/d/1cxLV4Pl66K7tSCXRcYLg_wsBUM3qk3hn/view?usp=sharing)
